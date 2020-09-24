85e08e0018b6af83b54eccf7ee3a8784d23fa841
tanaka0914 <tanaka.ryuya.0914@gmail.com>
2020-09-24 10:17:47 +0900

    プレイヤのLegかbulletoffsetあたりをいじったはず プレイヤがnonObjectでもジャンプできるようにした

--- TPSGame/Assets/Player/Off_Script/PlayerGround.cs
+++ TPSGame/Assets/Player/Off_Script/PlayerGround.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerGround : MonoBehaviour
 {
     [SerializeField] PlayerDirector S_Pdire;
-    string[] tagsname = new string[] { "Ground", "Object" };
+    string[] tagsname = new string[] { "Ground", "Object" , "NonObject" };
 
     void OnTriggerStay(Collider col) {  // コライダに触れている間
         for (int i = 0; i < tagsname.Length; i++) {
