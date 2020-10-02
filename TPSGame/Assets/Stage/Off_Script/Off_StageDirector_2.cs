1923ba5b8b672a5e61c35848c545c1ae3aca95e3
sho0630 <nkcgta.sho.takeuchi@gmail.com>
2020-10-02 11:29:42 +0900

    ステージ2生成

--- TPSGame/Assets/Stage/Off_Script/Off_StageDirector_2.cs
+++ TPSGame/Assets/Stage/Off_Script/Off_StageDirector_2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Off_StageDirector : MonoBehaviour
+public class Off_StageDirector_2 : MonoBehaviour
 {
     GameObject[] S_obj_;
     GameObject[] M_obj_;
@@ -10,11 +10,12 @@ public class Off_StageDirector : MonoBehaviour
     GameObject[] S_objpos;
     GameObject[] M_objpos;
     GameObject[] L_objpos;
+    GameObject[] GW_objpos;
+    GameObject[] GB_objpos;
     GameObject player_;
 
-    [SerializeField] GameObject[] stage;
-    [SerializeField] GameObject[] objpos_pre;
-
+    bool[] GW_pos = new bool[] {true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true};
+    bool[] GB_pos = new bool[] {true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true};
 
     GameObject respawn_1;//プレイヤーのスポーン位置
     GameObject respawn_2;
@@ -25,18 +26,21 @@ public class Off_StageDirector : MonoBehaviour
     [SerializeField] GameObject[] S_obj_1;
     [SerializeField] GameObject[] M_obj_1;
     [SerializeField] GameObject[] L_obj_1;
+    [SerializeField] GameObject[] GW_obj_1;
+    [SerializeField] GameObject[] GB_obj_1;
+    [SerializeField] GameObject[] respawn_obj;
+
 
     int[] S_rnd;
     int[] M_rnd;
     int[] L_rnd;
+    int GW_rnd;
+    int GB_rnd;
+    int respawn_rnd;
 
 
     void Start()
     {
-        Instantiate(stage[0]);
-        Instantiate(objpos_pre[0]);
-        respawn_1 = GameObject.Find("Respawn_1");
-        respawn_2 = GameObject.Find("Respawn_2");
         S_obj_ = S_obj_1;
         M_obj_ = M_obj_1;
         L_obj_ = L_obj_1;
@@ -46,13 +50,20 @@ public class Off_StageDirector : MonoBehaviour
         S_objpos = GameObject.FindGameObjectsWithTag("Small_Item");
         M_objpos = GameObject.FindGameObjectsWithTag("Medium_Item");
         L_objpos = GameObject.FindGameObjectsWithTag("Large_Item");
+        GW_objpos = GameObject.FindGameObjectsWithTag("siro");
+        GB_objpos = GameObject.FindGameObjectsWithTag("kuro");
 
         //置くものをランダムで決める
         objrnd();
         //ものの配置
+        respawnset(respawn_obj, GW_objpos, GB_objpos, respawn_rnd);
         Instobj(S_obj_, S_objpos, S_rnd);
         Instobj(M_obj_, M_objpos, M_rnd);
         Instobj(L_obj_, L_objpos, L_rnd);
+        InstGobj(GW_obj_1, GW_objpos, GW_pos);
+        InstGobj(GB_obj_1, GB_objpos, GB_pos);
+        respawn_1 = GameObject.Find("king1");
+        respawn_2 = GameObject.Find("king2");
         //プレイヤーの配置
         Instplayer(player_, respawn_1, respawn_2);
     }
@@ -75,19 +86,68 @@ public class Off_StageDirector : MonoBehaviour
         {
             L_rnd[i] = Random.Range(0, L_obj_.Length);
         }
+        respawn_rnd = Random.Range(0, 15);
+        GW_pos[respawn_rnd] = false;
+        GB_pos[respawn_rnd] = false;
+
+        for (int i = 0; i < 17; i++) 
+        {
+            GW_rnd = Random.Range(0, 23);
+            if (GW_pos[GW_rnd] == false)
+            {
+                i--;
+            }
+            else
+            {
+                GW_pos[GW_rnd] = false;
+            }
+        }
+        for(int i = 0; i < 17; i++)
+        {
+            GB_rnd = Random.Range(0, 23);
+            if (GB_pos[GB_rnd] == false)
+            {
+                i--;
+            }
+            else
+            {
+                GB_pos[GB_rnd] = false;
+            }
+        }
 
     }
 
     //オブジェクト生成
     void Instobj(GameObject[] obj, GameObject[] objpos, int[] rnd)
     {
-
         for (int i = 0; i < objpos.Length; i++)
         {
             Vector3 spawnpos = objpos[i].transform.position;
             Instantiate(obj[rnd[i]], spawnpos, obj[rnd[i]].transform.rotation * Quaternion.Euler(0.0f, Random.Range(0, 360), 0.0f));
             Destroy(objpos[i]);
         }
+    }
+
+    void InstGobj(GameObject[] Gobj, GameObject[] Gobjpos, bool[] pos)
+    {
+        for(int i = 0; i < Gobj.Length; i++)
+        {
+            for (int j = 0; j < Gobjpos.Length; j++)
+            {
+                if (pos[j] == true)
+                {
+                    Instantiate(Gobj[i], Gobjpos[j].transform.position, Gobjpos[j].transform.rotation);
+                    pos[j] = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    void respawnset(GameObject[] _respawn, GameObject[] _GW_pos, GameObject[] _GB_pos, int _respawn_rnd)
+    {
+        Instantiate(_respawn[0], _GW_pos[respawn_rnd].transform.position, Quaternion.identity);
+        Instantiate(_respawn[1], _GB_pos[respawn_rnd].transform.position, Quaternion.identity);
     }
 
     //プレイヤー・カメラ生成
