a4b47e57a84416eaf3b08d5038393f0f7c5e56f5
tanaka0914 <tanaka.ryuya.0914@gmail.com>
2020-10-07 12:44:32 +0900

    目的地に向かう機能修正

--- TPSGame/Assets/AI/Script/AIDirector.cs
+++ TPSGame/Assets/AI/Script/AIDirector.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class AIDirector : MonoBehaviour
 {
+    // AIの状態一覧
     enum AIState {
         WAIT,       // 待機中
         GOFLAG,     // 敵の旗を目指す
         GOHOME,     // 自分の陣地に帰る
         DEAD        // 死んだとき
     }
+    // AIの現在の状態
+    AIState nowState = AIState.WAIT;
 
     
     void Start()
@@ -19,6 +22,22 @@ public class AIDirector : MonoBehaviour
     
     void Update()
     {
-        
+
+        switch(nowState) {
+            case AIState.GOFLAG:
+
+                break;
+            case AIState.GOHOME:
+                
+                break;
+            case AIState.WAIT:
+                
+                break;
+            case AIState.DEAD:
+                
+                break;
+        }
+
+
     }
 }
