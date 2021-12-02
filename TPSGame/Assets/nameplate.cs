595d91772e0450e01700c39a95411d42ae404126
taka666 <nkcgta.takahiro.enjo@gmail.com>
2021-12-02 12:22:23 +0900

    playerの上にネームプレートを表示する処理 フェードインアウトの追加

---
+++ TPSGame/Assets/nameplate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nameplate : MonoBehaviour
+{
+    private RectTransform rect;
+    public Vector3 offset = new Vector3(0, 1.5f, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        rect.transform.position += offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rect.LookAt(Camera.main.transform.position);   
+    }
+    public void Setoffset(Vector3 vec)
+    {
+        rect.transform.position = vec;
+    }
+}
