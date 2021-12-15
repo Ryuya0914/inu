c7f88be6d00fec36b446d9b823c6f9a705d87aa2
hyuga <kurihara.73091@gmail.com>
2021-12-15 11:27:13 +0900

    aa

---
+++ TPSGame/Assets/Title/GameConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameConfiguration : MonoBehaviour
+{
+    public GameObject Configuration;
+    public GameObject canvas;
+    public GameObject taikistage;
+    public GameObject Stage_preset;
+    void Start()
+    {
+        Configuration.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            canvas.SetActive(false);
+            taikistage.SetActive(false);
+            Stage_preset.SetActive(false);
+            Configuration.SetActive(true);
+
+        }
+    }
+    
+}
---
+++ TPSGame/Assets/Title/Select.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Select : MonoBehaviour
+{
+    public Text TimeText;
+    private int Seconds;
+    private int Minutes;
+    void Start()
+    {
+        TimeText = GetComponent<Text>();
+        Seconds = 0;
+        Minutes = 5;
+        Ttext();
+        //TimeText.text = Minutes + ":" + Seconds;
+    }
+
+    void Update()
+    {
+        
+    }
+    public void Timer1()
+    {
+        Debug.Log("q");
+        Seconds += 30;
+        if (Seconds == 60)
+        {
+            Seconds = 0;
+            Minutes += 1;
+        }
+        Ttext();
+    }
+    void Ttext()
+    {
+        TimeText.text = Minutes + ":" + Seconds;
+    }
+}
